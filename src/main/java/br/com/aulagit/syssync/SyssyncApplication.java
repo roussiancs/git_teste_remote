d1ee72a9668e62ff0c61d345ed861e3c9cdd7616
Roussian Gaioso <roussian.comp@gmail.com>
2023-03-09 15:43:03 -0300

    Commite inicial

---
+++ src/main/java/br/com/aulagit/syssync/SyssyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulagit.syssync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyssyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyssyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aulagit/syssync/SyssyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aulagit.syssync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyssyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
